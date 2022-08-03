358813f09853a1ff49e021a579af723a53f5291e
Nikita <rioskill@yandex.ru>
2022-08-04 00:09:04 +0300

    fixed node_client

--- node_client/app.js
+++ node_client/app.js
@@ -1,41 +1,30 @@
 const net = require('net');
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
 
 let socket = new net.Socket();
 socket.connect(8080, '127.0.0.1');
 
 socket.on('connect', ()=>{
     console.log('connected');
-
-    read_and_write();
 });
 
-socket.on('data', (data)=>{
-    console.log(data);
-    const size = data.readInt32LE();
+socket.on('readable', ()=>{
+    const data = socket.read();
+
+    console.log(data.length);
+
+    const message_size = data.readInt32LE();
     const message = data.toString('utf-8', 4);
 
-    console.log('recieved: "' + message + '"');
+    console.log(data);
+    console.log(message_size + ' ' + message);
 });
 
-function read_and_write () {
-    rl.on("line", (message)=>{
-        const message_size = message.length;
-    
-        const buf = Buffer.alloc(4);
-        buf.writeInt32LE(message_size);
-    
-        console.log(buf);
-    
-        socket.write(buf);
-    
-        socket.write(message);
 
-        read_and_write();
-    });
-}
+process.stdin.on('data', (message) => {
+    message = message.slice(0, -1);
+    const buffer = Buffer.alloc(4);
+    buffer.writeInt32LE(message.length);
+
+    socket.write(buffer);
+    socket.write(message);
+});
